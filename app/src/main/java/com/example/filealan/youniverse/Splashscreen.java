88efb20a9c16b8a60a8607e6de6ca4a178967acb
alannahof <33405594+alannahof@users.noreply.github.com>
2018-06-03 17:46:02 +1200

    sunday_commit

---
+++ app/src/main/java/com/example/filealan/youniverse/Splashscreen.java
@@ -0,0 +1,21 @@
+package com.example.filealan.youniverse;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.util.Random;
+
+public class Splashscreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
